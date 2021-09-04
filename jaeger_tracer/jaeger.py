0535a9413948579753385670aabffeaefe28ba85
Janos Gerzson <gerzsonj@gmail.com>
2021-09-04 21:43:36 +0200

    [IMP] jaeger_tracing 12.0

--- jaeger_tracer/jaeger.py
+++ jaeger_tracer/jaeger.py
@@ -1,37 +1,37 @@
 # coding: utf-8
 
 import functools
-import opentracing.tracer
+import opentracing
 
 
 def span(_func=None, *,
-         _operation='', _tags={}, _log_kv={}):
+         operation='', tags={}, log_kv={}):
     '''Decorator jaeger.span accepts the following arguments:
-    _operation (str) - operation name,
-    _tags (dict) - tag key/value pairs,
-    _log_kv (dict) - structured log, forwarded to the span
+    operation (str) - operation name,
+    tags (dict) - tag key/value pairs,
+    log_kv (dict) - structured log, forwarded to the span
     '''
 
     def decorator_span(func):
         @functools.wraps(func)
         def _span(*args, **kwargs):
             # filling span properties
-            tags = _tags
-            log_kv = _log_kv
-            operation = func.__name__ if not _operation else _operation
+            _tags = tags
+            _log_kv = log_kv
+            _operation = func.__name__ if not operation else operation
 
-            if 'module' not in tags.keys():
-                tags['module'] = func.__module__
-            if 'event' not in log_kv.keys() and func.__doc__:
-                log_kv['event'] = func.__doc__
+            if 'module' not in _tags.keys():
+                _tags['odoo.module'] = func.__module__
+            if 'event' not in _log_kv.keys() and func.__doc__:
+                _log_kv['event'] = func.__doc__
 
             tracer = opentracing.tracer
-            with tracer.start_active_span(operation) as scope:
+            with tracer.start_active_span(_operation) as scope:
                 # set tags for span
-                for _ in tags.items():
+                for _ in _tags.items():
                     scope.span.set_tag(_[0], _[1])
                 # set log for span
-                scope.span.log_kv(log_kv)
+                scope.span.log_kv(_log_kv)
 
                 ret = func(*args, **kwargs)
 
