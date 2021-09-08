bee5ff800584e253787d938cef23353f1d392ace
Janos Gerzson <gerzsonj@gmail.com>
2021-09-08 12:52:46 +0200

    [IMP] new module: jaeger_tracer_instrumentation_controllers

---
+++ jaeger_tracer_instrumentation_controllers/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Jaeger Tracer Instrumantation Controllers",
+    'summary': "Jaeger Instrumantation for http requests",
+    'author': "János Gerzson (@grzs)",
+    'website': "https://github.com/grzs/odoo-addons/tree/14.0/jaeger_tracer_instrumentation_controllers",
+    'license': 'LGPL-3',
+    'category': 'Technical',
+    'version': '0.1',
+    'depends': ['jaeger_tracer'],
+    'data': [],
+}
